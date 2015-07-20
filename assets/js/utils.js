be60fd7275d1016abfb4072855f975cb89b0ae65
Segonquart Studio {public repositories} <delfin@segonquart.net>
2015-07-20 09:43:46 +0200

    utils and format

---
+++ assets/js/utils.js
@@ -0,0 +1,10 @@
+
+
+if ((window.location.href.toString().indexOf(&#39;.com.fr/&#39;))&gt;&#39;1&#39;)
+        {
+        window.location.href=window.location.href.toString().replace(&#39;.blogspot.com.fr/&#39;, &#39;.blogspot.com/ncr/&#39;);
+                }
+
+else {
+log ();
+        }
