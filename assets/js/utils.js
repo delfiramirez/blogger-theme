75cbf1824b932188ef0bca3095a670e06a6ba30e
Segonquart Studio {public repositories} <delfin@segonquart.net>
2015-07-20 10:06:39 +0200

    reformat

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,17 @@
 
+// prototypes
+
+if (!Array.indexOf)
+{
+Array.prototype.indexOf=function (obj, start)
+{
+for (var i=(start||0);i<this.length;i++)
+{ if (this[i]==obj) { return i;} }
+return-1;
+}
+}
+
+// custom domain
 
 if ((window.location.href.toString().indexOf(&#39;.com.fr/&#39;))&gt;&#39;1&#39;)
         {
